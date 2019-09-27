90ecf6b7c872592974130f204b4a30837964b433
Nikunj Ramani <30588992+nikunjramani@users.noreply.github.com>
2019-09-27 23:20:56 +0530

    Add files via upload

---
+++ image.py
@@ -0,0 +1,44 @@
+import cv2
+import sys
+import os
+"""datasets = 'ii'  
+sub_data = 'nikunj'     
+  
+path = os.path.join(datasets, sub_data) 
+if not os.path.isdir(path): 
+    os.mkdir(path)""" 
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+video_capture = cv2.VideoCapture(0)
+
+count=1
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(100, 100)
+    )
+    # Draw a rectangle around the faces
+    padding=50
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame,(x-padding,y-padding),(x+w+padding,y+h+padding),(255,0,0),2)
+        face=frame[y-50:y+h+50, x-50:x+w+50] 
+        face_resize = cv2.resize(face, (1000, 1000)) 
+        cv2.imwrite('a.png', face_resize) 
+        #count += 1
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
